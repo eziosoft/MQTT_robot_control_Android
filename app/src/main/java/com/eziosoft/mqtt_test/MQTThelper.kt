11027f148d0ae522c6aac33dcf89643ecdabaf06
Bart <eziosoft@gmail.com>
2019-05-24 23:33:15 +0200

    Licence

--- app/src/main/java/com/eziosoft/mqtt_test/MQTThelper.kt
+++ app/src/main/java/com/eziosoft/mqtt_test/MQTThelper.kt
@@ -1,3 +1,23 @@
+/*
+ *  This file is part of MQTT_robot_control_Android.
+ *
+ *     MQTT_robot_control_Android is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Foobar is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2019. Bartosz Szczygiel
+ *
+ */
+
 package com.eziosoft.mqtt_test
 
 import android.content.Context
@@ -6,10 +26,6 @@ import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
 
 
-/**
- * Written by Bartosz Szczygiel <eziosoft@gmail.com>
- * Created on 12/02/2019.
- */
 
 
 class MqttHelper {
